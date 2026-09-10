c7ba8e5ff24b21f3d9c8531c86b8d23e290c0fb0
Rushita pawar <rushita226@gmail.com>
2026-09-10 15:07:37 +0530

    Fix ListController import casing

--- backend/routes/ListRoutes.js
+++ backend/routes/ListRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createList, getLists } = require('../controllers/listController');
+const { createList, getLists } = require('../controllers/ListController');
 const protect = require('../middleware/authMiddleware');
 
 router.post('/', protect, createList);
